bf29b75a24e7edefb6ca653129cc2c16efc2a4b0
RIZQIZIA5713 <muhammadrizqiziaulia@gmail.com>
2024-10-24 20:13:29 +0700

    menambahkan responsive

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
+// NAV
+const navItems = document.querySelector('nav');
+const openNavBtn = document.querySelector('#open_nav-btn');
+const closeNavBtn = document.querySelector('#close_nav-btn');
+
+const openNav = () => {
+    navItems.style.display = 'flex';
+    openNavBtn.style.display = 'none';
+    closeNavBtn.style.display = 'inline-block';
+}
+
+const closeNav = () => {
+    navItems.style.display = 'none';
+    openNavBtn.style.display = 'inline-block';
+    closeNavBtn.style.display = 'none';
+}
+
+openNavBtn.addEventListener('click', openNav);
+closeNavBtn.addEventListener('click', closeNav);
+
+// CAROUSEL
 let carousel = document.querySelector('.carousel .carousel-slider');
 let slider = document.querySelectorAll('.carousel .carousel-slider .slider');
 let next = document.getElementById('next');
